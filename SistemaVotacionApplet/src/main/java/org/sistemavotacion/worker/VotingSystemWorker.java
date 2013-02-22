bb3737d75ed6fa8a78f39cbd9e07916069628c7b
JJ <jgzornoza@gmail.com>
2013-02-22 22:36:20 +0100

    Reparaciones

---
+++ SistemaVotacionApplet/src/main/java/org/sistemavotacion/worker/VotingSystemWorker.java
@@ -0,0 +1,13 @@
+package org.sistemavotacion.worker;
+
+/**
+* @author jgzornoza
+* Licencia: https://raw.github.com/jgzornoza/SistemaVotacionAppletFirma/master/licencia.txt
+*/
+public interface VotingSystemWorker {
+    
+    public String getMessage();
+    public int getId();
+    public int getStatusCode();
+    
+}
