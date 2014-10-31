33c41a9610f03ec45429e393a9d92e040e463700
votingsystem <1githubsotopo2>
2014-10-31 09:36:20 +0100

    bug repairs

--- AccessControl/grails-app/conf/BuildConfig.groovy
+++ AccessControl/grails-app/conf/BuildConfig.groovy
@@ -80,13 +80,13 @@ grails.project.dependency.resolution = {
 
         // plugins for the compile step
         compile ":scaffolding:2.1.0"
-        compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.9.6"
+        compile ':cache:1.1.8'
+        compile ":asset-pipeline:1.9.9"
         compile ':executor:0.3'
         compile ':rendering:1.0.0'
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.15"
+        runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.15"
         runtime ":database-migration:1.4.0"
 
         // Uncomment these to enable additional asset-pipeline capabilities
--- Vickets/grails-app/conf/BuildConfig.groovy
+++ Vickets/grails-app/conf/BuildConfig.groovy
@@ -82,13 +82,13 @@ grails.project.dependency.resolution = {
 
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
-        compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.9.6"
+        compile ':cache:1.1.8'
+        compile ":asset-pipeline:1.9.9"
         compile ":quartz:1.0.1"
         compile ":executor:0.3"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.15"
+        runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.15"
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.7.4"
