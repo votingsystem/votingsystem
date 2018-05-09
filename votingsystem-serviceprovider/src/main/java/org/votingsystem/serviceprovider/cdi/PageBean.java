024011f3963a745010a60aaf378678d26512e4f0
jgzornoza <jgzornoza@gmail.com>
2018-05-09 11:20:58 +0200

    updates

---
+++ votingsystem-serviceprovider/src/main/java/org/votingsystem/serviceprovider/cdi/PageBean.java
@@ -0,0 +1,114 @@
+package org.votingsystem.serviceprovider.cdi;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.omnifaces.cdi.Push;
+import org.omnifaces.cdi.PushContext;
+import org.votingsystem.dto.metadata.MetadataDto;
+import org.votingsystem.ejb.Config;
+import org.votingsystem.ejb.TrustedServicesEJB;
+import org.votingsystem.http.SystemEntityType;
+import org.votingsystem.jsf.ServiceUpdatedMessage;
+
+import org.votingsystem.util.DateUtils;
+import org.votingsystem.util.JSON;
+import org.votingsystem.util.Messages;
+
+import javax.ejb.EJB;
+import javax.enterprise.event.Observes;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Helper class to hold variables and methods needed by some web pages
+ */
+@Named("pageBean")
+@ApplicationScoped
+public class PageBean implements Serializable {
+
+    private static final Logger log = Logger.getLogger(PageBean.class.getName());
+
+    @EJB
+    Config config;
+    @EJB
+    TrustedServicesEJB trustedServices;
+    @Inject @Push
+    private PushContext serviceUpdated;
+
+    public void serviceUpdated(@Observes ServiceUpdatedMessage serviceUpdatedMessage) {
+        log.log(Level.INFO, "uuid: " + serviceUpdatedMessage.getClientUUID());
+        try {
+            serviceUpdated.send(JSON.getMapper().writeValueAsString(serviceUpdatedMessage.getResponse()),
+                    serviceUpdatedMessage.getClientUUID());
+        } catch (Exception ex) {
+            log.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+
+    /**
+     * Method that format a Date with the pattern yyyy-MM-dd HH:mm:ss
+     *
+     * @param date
+     * @return the formatted date
+     */
+    public String formatDate(Date date) {
+        if(date == null) return "";
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return formatter.format(date);
+    }
+
+    public String formatDate(LocalDateTime date) {
+        if(date == null) return "";
+        return DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(ZonedDateTime.of(date, ZoneId.systemDefault()));
+    }
+
+    public String formatDate(ZonedDateTime date) {
+        if(date == null) return "";
+        return DateUtils.getDateStr(date);
+    }
+
+    public String applicationCodeMsg(String applicationCodeStr) {
+        return Messages.currentInstance().get(applicationCodeStr);
+    }
+
+    public String locale() {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        return request.getLocale().toString();
+    }
+
+    public String now() {
+        return formatDate(new Date());
+    }
+
+    public String getVotingServicesCountryMapAsJSON() throws JsonProcessingException {
+        Set<MetadataDto> votingServices = trustedServices.getEntitySetByType(SystemEntityType.VOTING_SERVICE_PROVIDER);
+        return getCountryMapAsJSON(votingServices);
+    }
+
+    public String getIdProvidersCountryMapAsJSON() throws JsonProcessingException {
+        Set<MetadataDto> idProviders = trustedServices.getEntitySetByType(SystemEntityType.ID_PROVIDER);
+        return getCountryMapAsJSON(idProviders);
+    }
+
+    private String getCountryMapAsJSON(Set<MetadataDto> entities) throws JsonProcessingException {
+        Map<String, String> resultMap = new HashMap<>();
+        for(MetadataDto metadataDto : entities) {
+            String countryCode = metadataDto.getEntity().getLocation().getCountry().getCode();
+            resultMap.put(countryCode, new Locale("", countryCode).getDisplayCountry());
+        }
+        return JSON.getMapper().writeValueAsString(resultMap);
+    }
+
+}
---
+++ votingsystem-serviceprovider/src/main/java/org/votingsystem/serviceprovider/cdi/PublishElectionBean.java
@@ -0,0 +1,193 @@
+package org.votingsystem.serviceprovider.cdi;
+
+import com.google.zxing.WriterException;
+import org.votingsystem.dto.voting.ElectionDto;
+import org.votingsystem.ejb.Config;
+import org.votingsystem.ejb.QRSessionsEJB;
+import org.votingsystem.model.voting.Election;
+import org.votingsystem.model.voting.ElectionOption;
+import org.votingsystem.qr.QRRequestBundle;
+import org.votingsystem.qr.QRUtils;
+import org.votingsystem.util.Constants;
+import org.votingsystem.util.OperationType;
+import org.votingsystem.xml.XML;
+
+import javax.ejb.AccessTimeout;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.Serializable;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * License: https://github.com/votingsystem/votingsystem/wiki/Licencia
+ *
+ * Helper class to hold variables and methods needed by some web pages
+ */
+@SessionScoped
+@Named("publishElection")
+@AccessTimeout(value = 10, unit = TimeUnit.MINUTES)
+public class PublishElectionBean implements Serializable {
+
+    private static final Logger log = Logger.getLogger(PublishElectionBean.class.getName());
+
+    private static final String ELECTION_OPTIONS_SEPARATOR = "#@#@#@";
+
+    @PersistenceContext
+    private EntityManager em;
+    @Inject private Config config;
+    @EJB private QRSessionsEJB qrSessions;
+
+    private String electionContent;
+    private String electionDate;
+    private String electionUUID;
+    private String errorMsg;
+    private String electionSubject;
+    private String electionOptions;
+    private String qrId;
+    private String qrUUID;
+
+
+    public String onload() {
+        if(electionUUID != null) {
+            List<Election> electionList = em.createQuery("select e from Election e where e.uuid=:electionUUID")
+                    .setParameter("electionUUID", electionUUID).getResultList();
+            if(!electionList.isEmpty()) {
+                resetForm();
+            }
+        }
+        return null;
+    }
+
+    public String publish() {
+        errorMsg = null;
+        try {
+            HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+            Set<ElectionOption> electionOptionSet = new HashSet<>();
+            for(String electionOption : electionOptions.split(ELECTION_OPTIONS_SEPARATOR)){
+                electionOptionSet.add(new ElectionOption(electionOption));
+            }
+            //All elections begin at 00:00 GMT+00:00
+            LocalDate localDate = LocalDate.parse(electionDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            ZonedDateTime zonedDateBegin = localDate.atStartOfDay(ZoneOffset.UTC).withZoneSameInstant(ZoneId.systemDefault());
+            LocalDateTime dateBegin = zonedDateBegin.toLocalDateTime();
+            log.info("electionSubject: " + electionSubject + " - dateBegin: " + dateBegin);
+
+            Election election = new Election(electionSubject, electionContent, dateBegin, dateBegin.plusDays(1),
+                    Election.State.PENDING);
+            electionUUID = UUID.randomUUID().toString();
+            election.setUUID(electionUUID).setElectionOptions(electionOptionSet);
+
+            ElectionDto electionDto = new ElectionDto(election);
+            electionDto.setEntityId(config.getEntityId());
+
+            byte[] reqBytes = XML.getMapper().writeValueAsBytes(electionDto);
+            log.info("reqBytes: " + new String(reqBytes));
+            //XMLValidator.validatePublishElectionRequest(reqBytes);
+
+            qrUUID = election.getUUID();
+            qrId = qrUUID.substring(0, 4).toUpperCase();
+            Set<String> qrSessionsSet = (Set<String>) req.getSession().getAttribute(Constants.QR_OPERATIONS);
+            if(qrSessionsSet == null)
+                qrSessionsSet = new HashSet<>();
+            qrSessionsSet.add(qrUUID);
+            qrSessions.putOperation(qrUUID, new QRRequestBundle<>(OperationType.PUBLISH_ELECTION, electionDto));
+            return "/publish-election/publish.xhtml?faces-redirect=true&includeViewParams=true";
+        } catch (Exception ex) {
+            log.log(Level.SEVERE, ex.getMessage(), ex);
+            errorMsg = ex.getMessage();
+            return "editor";
+        }
+    }
+
+    public String getQRCodeURL() throws WriterException, IOException {
+        HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String qrCodeURL = req.getContextPath() + "/api/qr?cht=qr&chs=200x200&chl=" +
+                QRUtils.SYSTEM_ENTITY_KEY + "=" + config.getEntityId() + ";" +
+                QRUtils.UUID_KEY + "=" + qrUUID + ";";
+        return qrCodeURL;
+    }
+
+    private void resetForm() {
+        electionContent = null;
+        electionSubject = null;
+        electionDate = null;
+        electionUUID = null;
+        errorMsg = null;
+        electionOptions = null;
+        qrId = null;
+        qrUUID = null;
+    }
+
+    public String getElectionContent() {
+        return electionContent;
+    }
+
+    public void setElectionContent(String electionContent) {
+        this.electionContent = electionContent;
+    }
+
+    public String getElectionDate() {
+        return electionDate;
+    }
+
+    public void setElectionDate(String electionDate) {
+        this.electionDate = electionDate;
+    }
+
+    public String getElectionSubject() {
+        return electionSubject;
+    }
+
+    public void setElectionSubject(String electionSubject) {
+        this.electionSubject = electionSubject;
+    }
+
+    public String getElectionOptions() {
+        return electionOptions;
+    }
+
+    public void setElectionOptions(String electionOptions) {
+        this.electionOptions = electionOptions;
+    }
+
+    public String getQrId() {
+        return qrId;
+    }
+
+    public void setQrId(String qrId) {
+        this.qrId = qrId;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public String getQrUUID() {
+        return qrUUID;
+    }
+
+    public String getElectionUUID() {
+        return electionUUID;
+    }
+
+    public void setElectionUUID(String electionUUID) {
+        this.electionUUID = electionUUID;
+    }
+
+}
